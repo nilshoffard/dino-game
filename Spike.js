bced36810b75cd410b6f782a03c46a560576639c
nilshoffard <dr.rock1312@gmail.com>
2023-08-24 23:59:05 +0200

    Added live counter collision rework

--- Spike.js
+++ Spike.js
@@ -11,8 +11,10 @@ class Spike {
     }
     
     show() {
-        image(sImg, this.x, this.y, (this.r * 2)/3, this.r);
-        fill(255,50);
-        rect(this.x, this.y, (this.r * 2)/3, this.r);
+        image(sImg, this.x, this.y, this.r, this.r);
+        // fill(255,50);
+        // rect(this.x + ((this.r * 1)/4), this.y, (this.r * 2)/4, this.r);
+        // rect(this.x, this.y, (this.r * 2)/4, this.r);
+
     }
 }
